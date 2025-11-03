7b123c68a27ccc31b062e380f80d2192f0be0290
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-11-03 13:55:25 -0500

    refactor: clean up App component by removing commented code and unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-// src/App.tsx
 import { ToastContainer } from 'react-toastify';
-import { AppRouter } from './routes/AppRoutes'; // Ruta relativa
-
-// --- IMPORTS DE ESTILO ---
-// Siguiendo el patrón de sistema-de-gestion/src/App.jsx
-import 'react-toastify/dist/ReactToastify.css'; // Toastify (ruta corregida)
-// Los otros estilos se movieron a main.tsx
+import { AppRouter } from './routes/AppRoutes';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
-      {/* AppRouter ya no está comentado.
-QueryClientProvider se quitó como pediste.
-*/}
       <AppRouter />
 
       {/* Mantenemos el ToastContainer aquí */}
