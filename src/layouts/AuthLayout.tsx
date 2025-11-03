c71b586020eb1f87b3a51f80e55beeb1a1fe9334
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-11-03 13:56:46 -0500

    refactor: clean up AuthLayout component by removing comments and reorganizing class names

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,15 +2,10 @@ import { Outlet } from 'react-router-dom';
 import { Footer } from './includes/Footer';
 import { Navbar } from './includes/Navbar';
 
-/**
- * Layout para las páginas públicas (Home, Login, Register, etc.)
- * Tiene un Navbar público y un Footer.
- */
 export function AuthLayout() {
   return (
-    <div className="relative flex min-h-screen flex-col bg-background">
+    <div className="relative flex flex-col bg-background min-h-screen">
       <Navbar />
-      {/* Contenido principal (HomePage, LoginPage, etc.) */}
       <main className="flex-1">
         <Outlet />
       </main>
