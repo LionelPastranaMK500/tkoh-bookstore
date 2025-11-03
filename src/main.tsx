1a77cc1e4bda9603b80c8cad9ac14592a9ca9ca9
LavenderEdit <gercermagden@gmail.com>
2025-11-03 15:51:50 -0500

    refactor: remove PrimeReact dependencies and styles, update Tailwind CSS configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { PrimeReactProvider } from 'primereact/api';
 import App from './App';
-
-// import 'primereact/resources/primereact.min.css';
-// import 'primeicons/primeicons.css';
-import './styles/primereact-styles.css';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PrimeReactProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </PrimeReactProvider>
   </React.StrictMode>,
 );
