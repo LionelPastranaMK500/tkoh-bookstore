17c236de96afe4bead298f16ff0202a7e33bc22c
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-11-03 13:56:55 -0500

    refactor: clean up MainLayout component by removing comments and reorganizing class names

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,19 +3,13 @@ import { Footer } from './includes/Footer';
 import { Header } from './includes/Header';
 import { Sidebar } from './includes/Sidebar';
 
-/**
- * Layout para las páginas privadas (Dashboard, Admin, etc.)
- * Tiene un Header, Sidebar, Footer y un <Outlet> para el contenido.
- * Logra la experiencia "unipaginal" (SPA).
- */
 export default function MainLayout() {
   return (
-    <div className="relative flex min-h-screen flex-col bg-background">
+    <div className="relative flex flex-col bg-background min-h-screen">
       <Header />
-      <div className="container flex-1 items-start md:grid md:grid-cols-[240px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[260px_minmax(0,1fr)] lg:gap-10">
+      <div className="flex-1 items-start md:gap-6 lg:gap-10 md:grid md:grid-cols-[240px_minmax(0,1fr)] lg:grid-cols-[260px_minmax(0,1fr)] container">
         <Sidebar />
         <main className="relative py-6 lg:py-8">
-          {/* Aquí se renderiza el contenido dinámico privado (AdminPage, etc.) */}
           <Outlet />
         </main>
       </div>
